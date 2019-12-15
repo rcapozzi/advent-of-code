b3152107b81b2b56b727af21f98d99e6ccb2b9a5
Raymond Capozzi <raymond.capozzi@shopify.com>
2019-12-14 22:24:36 -0500

    Add test for Day

--- src/2019-07.rb
+++ src/2019-07.rb
@@ -1,6 +1,7 @@
 # require './src/2019-05.rb'
-
+require 'test/unit'
 require 'byebug'
+
 class Instruction
 	attr_reader :opcode, :p1, :p2, :p3, :ptr
 
@@ -159,30 +160,29 @@ class IntComputer
 end
 
 def maxamp(program, data)
+	ic = IntComputer.new
 	result = 0
 	data.each do |amp|
-		ic = IntComputer.new
 		ic.input = [amp, result]
 		result = ic.execute(program)
 	end
-	result
 	puts "output: %s" % result
+	result
 end
 
 if __FILE__ == $0
+	extend Test::Unit::Assertions
+
 	filename = ARGV[0] || 'data/2019-05.input.txt'
 	program = File.read(filename).chomp
 
 	program = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0'
 	data = [4,3,2,1,0]
-	expect = 43210
-	maxamp(program, data)
+	assert_equal 43210, maxamp(program, data)
 
 	data = [0, 1, 2, 3, 4]
-	expect = 554321
-	maxamp('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0', data)
+	assert_equal 54321, maxamp('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0', data)
 
 	data = [1,0,4,3,2]
-	expect = 65210
-	maxamp('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', data)
+	assert_equal 65210, maxamp('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', data)
 end
